4e2e60a4c27cdc77c92dc815221cb7065f6ae7c1
MartinAlbanesi <martinalbanesi89@gmail.com>
2022-04-28 15:41:40 -0300

    Se crean los metodos de calculadora y se utilizan en el Switch Case

--- Calculadora.java
+++ Calculadora.java
@@ -22,33 +22,51 @@ public static void main(String[] args) {
 	b = teclado.nextInt();
  
 	//Calculo de operaciones según opcion ingresada
-	/*
-	if(opcion.equals(1)){
-		resultado = a + b; 
-	}else if(opcion.equals(2)){
-		resultado = a - b;
-	}else if(opcion.equals(3)){
-		resultado = a * b;
-	}else{
-		resultado = a / b;
-	}
-	*/
-	
 	switch (opcion) {
-	case 1:
-		resultado = a * b;
-		break;
-	case 2:
-		resultado = a / b;
-		break;
-	case 3:
-		resultado = a + b; 
-		break;
-	default:
-		resultado = a - b;
+		case 1:
+			resultado = multiplicar(a,b);
+			break;
+		case 2:
+			resultado = dividir(a,b);
+			break;
+		case 3:
+			resultado = sumar(a,b);
+			break;
+		default:
+			resultado = restar(a,b);
 	}
  	
 	System.out.println("El resultado es " + resultado.toString());
 
 	}
+
+//Metodos de Calculadora
+public static Integer sumar(Integer a, Integer b) {
+	return a + b;
 }
+
+public static Integer restar(Integer a, Integer b) {
+	return a - b;
+}
+
+public static Integer multiplicar(Integer a, Integer b) {
+	return a * b;
+}
+
+public static Integer dividir(Integer a, Integer b) {
+	return a / b;
+}
+
+}
+
+/*
+ * Al realizar el segundo push, me tiró el siguiente error "fatal: Could not read from remote repository."
+ * Este error fue debido a que puse la dirección SSH en lugar de HTTPS al settear la URL del repositorio remoto
+ * Luego me tiró el siguiente error "Invalid username or password."
+ * Logre solucionarlo utilizando el siguiente comando "git config --global user.name" y creando un token de acceso personal.
+ * Al realizar el push me pidió la contraseña y al usar el token logré realizarlo sin problemas.
+ * Para evitar el problema en un futuro es recomentable configurar los datos de git con el comando "git config" y crear un token de acceso personal (con o sin vencimiento)
+ 
+ *El parámetro -b se refiere al termino branch (rama en español), en este caso se utiliza "git checkout -b develop" para crear una nueva rama llamada "develop"
+ */
+
